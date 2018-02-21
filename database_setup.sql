a113643155d450e6e7a9a4eaddb4f77acea3e054
Justin Brockbank <justinsbrockbank@gmail.com>
2018-02-20 18:08:21 -0700

    Added product table, still needs work.

--- database_setup.sql
+++ database_setup.sql
@@ -1,8 +1,21 @@
 CREATE TABLE customer (
 	id INT AUTO_INCREMENT PRIMARY KEY, 
 	name VARCHAR(255),
+	street VARCHAR(255),
+	city VARCHAR(255),
+	state VARCHAR(255),
+	zip INT(9),
+	phone INT(20),
 	email VARCHAR(255),
+	password VARCHAR(255),
+	newsletter BOOLEAN,
 	registration_date TIMESTAMP,
 );
 
-mysql data types for searching
+
+
+
+CREATE TABLE product (
+	serial_number VARCHAR(255),
+	price DECIMAL(7,2),
+);
