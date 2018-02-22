0b6bea99f93687a54577e057c75ed60eae006620
Justin Brockbank <justinsbrockbank@gmail.com>
2018-02-22 10:17:58 -0700

    Added admin webpage and more tables to database_setup.

--- database_setup.sql
+++ database_setup.sql
@@ -1,5 +1,5 @@
 CREATE TABLE customer (
-	id INT AUTO_INCREMENT PRIMARY KEY, 
+	customer_id INT AUTO_INCREMENT PRIMARY KEY, 
 	name VARCHAR(255),
 	street VARCHAR(255),
 	city VARCHAR(255),
@@ -12,10 +12,50 @@ CREATE TABLE customer (
 	registration_date TIMESTAMP,
 );
 
-
-
+CREATE TABLE order (
+	order_number INT AUTO_INCREMENT PRIMARY KEY,
+	order_date TIMESTAMP,
+	to_street,
+	to_city,
+	to_state,
+	to_zip,
+	billed_cc_number,
+	billed_cc_expiration,
+	order_total,
+	customer_id INT FOREIGN KEY,
+);
 
 CREATE TABLE product (
-	serial_number VARCHAR(255),
+	serial_number VARCHAR(255) PRIMARY KEY,
 	price DECIMAL(7,2),
+	color,
+	decription,
+	image,
+	ram,
+	storage,
+	sold,
+	order_number FOREIGN KEY,
 );
+
+CREATE TABLE watch (
+	serial_number VARCHAR(255) FOREiGN KEY,
+	series,
+);
+
+CREATE TABLE phone (
+	serial_number VARCHAR(255) FOREiGN KEY,	
+	model,
+);
+
+CREATE TABLE laptop (
+	serial_number VARCHAR(255) FOREiGN KEY,
+	year,
+	version,
+);
+
+CREATE TABLE streaming_device (
+	serial_number VARCHAR(255) FOREiGN KEY,
+	model,
+);
+
+
