7081c0bb450acda257fa5ecc5c06d82cc181f8e0
Justin Brockbank <justinsbrockbank@gmail.com>
2018-02-28 19:37:56 -0700

    Updates to help file.

--- adminPage.php
+++ adminPage.php
@@ -1,6 +1,21 @@
 <?php
 
-
-
+session_start();
+define(ADMIN, $_SESSION['name']);
+if(!session_is_registered("admin")){
+	header("location:adminLoginPage.php");
+} 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<center>
+<h1>Buy My Apple</h1>
+<h2>Welcome <?php echo ADMIN?> to the Admin Webpage</h2>
+<p><a href="logoutPage.php">Logout?</a></p>
+</center>
+
+
