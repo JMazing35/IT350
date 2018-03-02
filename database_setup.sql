49a8d6600d0169061b282668b3b4c0b445ac0bbe
Justin Brockbank <justinsbrockbank@gmail.com>
2018-03-01 21:00:45 -0700

    Added info to database_setup.sql

--- database_setup.sql
+++ database_setup.sql
@@ -1,61 +1,83 @@
+CREATE DATABASE buyMyApple; #Might not be necessary
+
 CREATE TABLE customer (
-	customer_id INT AUTO_INCREMENT PRIMARY KEY, 
-	name VARCHAR(255),
-	street VARCHAR(255),
-	city VARCHAR(255),
-	state VARCHAR(255),
-	zip INT(9),
-	phone INT(20),
-	email VARCHAR(255),
-	password VARCHAR(255),
-	newsletter BOOLEAN,
+	customer_id INT (8) AUTO_INCREMENT PRIMARY KEY, 
+	name VARCHAR(255) NOT NULL,
+	street VARCHAR(255) NOT NULL,
+	city VARCHAR(255) NOT NULL,
+	state VARCHAR(255) NOT NULL,
+	zip INT(9) NOT NULL,
+	phone INT(20) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	newsletter BOOLEAN default 0,
 	registration_date TIMESTAMP,
 );
 
 CREATE TABLE order (
-	order_number INT AUTO_INCREMENT PRIMARY KEY,
+	order_number INT(8) AUTO_INCREMENT PRIMARY KEY,
 	order_date TIMESTAMP,
-	to_street,
-	to_city,
-	to_state,
-	to_zip,
-	billed_cc_number,
-	billed_cc_expiration,
-	order_total,
-	customer_id INT FOREIGN KEY,
+	to_street VARCHAR(255) NOT NULL,
+	to_city VARCHAR(255) NOT NULL,
+	to_state VARCHAR(255) NOT NULL,
+	to_zip VARCHAR(255) NOT NULL, 
+	billed_cc_number INT(16) NOT NULL,
+	billed_cc_expiration INT(4) NOT NULL, #0322 for example
+	order_total DECIMAL (7,2) NOT NULL,
+	FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
 CREATE TABLE product (
 	serial_number VARCHAR(255) PRIMARY KEY,
-	price DECIMAL(7,2),
-	color,
-	decription,
-	image,
-	ram,
-	storage,
-	sold,
-	order_number FOREIGN KEY,
+	price DECIMAL(7,2) NOT NULL,
+	color VARCHAR (20) NOT NULL, 
+	decription VARCHAR (255) NOT NULL,
+	ram VARCHAR (255) NOT NULL,
+	storage VARCHAR (255) NOT NULL,
+	sold BOOLEAN default 0,
+	FOREIGN KEY (order_number) REFERENCES order(order_number)
 );
 
 CREATE TABLE watch (
-	serial_number VARCHAR(255) FOREiGN KEY,
-	series,
+	FOREiGN KEY (serial_number) REFERENCES product(serial_number),
+	series VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE phone (
-	serial_number VARCHAR(255) FOREiGN KEY,	
-	model,
+	FOREiGN KEY (serial_number) REFERENCES product(serial_number),	
+	model VARCHAR (255) NOT NULL
 );
 
 CREATE TABLE laptop (
-	serial_number VARCHAR(255) FOREiGN KEY,
-	year,
-	version,
+	FOREiGN KEY (serial_number) REFERENCES product(serial_number),
+	year INT(4) NOT NULL,
+	version VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE streaming_device (
-	serial_number VARCHAR(255) FOREiGN KEY,
-	model,
+	FOREiGN KEY (serial_number) REFERENCES product(serial_number),
+	model VARCHAR (255) NOT NULL
+);
+
+
+CREATE TABLE employee (
+	employee_id INT (8) AUTO_INCREMENT PRIMARY KEY,
+	ssn INT(9) PRIMARY KEY,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	street VARCHAR(255) NOT NULL,
+	city VARCHAR(255) NOT NULL,
+	state VARCHAR(255) NOT NULL,
+	zip INT(9) NOT NULL,
+	phone INT(20) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE department (
+	department_id INT(8) AUTO_INCREMENT PRIMARY KEY,
+	department_name VARCHAR(255) NOT NULL,
+	FOREIGN KEY (manager_employee_id) REFERENCES employee(employee_id) 
 );
 
 
