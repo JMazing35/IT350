c4528f347c955f091f30f45c3a76e1dc9a124a2a
JMazing35 <justinsbrockbank@gmail.com>
2018-03-06 20:51:53 -0700

    Update adminPage.php

--- adminPage.php
+++ adminPage.php
@@ -19,4 +19,44 @@ if(!session_is_registered("admin")){
 <p><a href="logoutPage.php">Logout?</a></p>
 </center>
 
+<br>
+<br>
+<br>
+	
+<h1>List of Employees</h1>
+<table class="list">
+	<tr>
+		<th>Employee ID</th>
+		<th>First Name</th>
+		<th>Last Name</th>
+		<th>Street</th>
+		<th>City</th>
+		<th>State</th>
+		<th>Zip</th>
+		<th>Phone</th>
+		<th>Email</th>
+	</tr>
+	<?php
+	$sql = "SELECT * FROM employee";
+	$dbConnection = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName);
+	$dataSet = mysqli_query($dbConnection, $sql);
+	
+	while ($data = mysqli_fetch_assoc($dataSet)) { ?>
+		<tr>
+			<td><?php echo h($data['employee_id']); ?></td>
+			<td><?php echo h($data['first_name']); ?></td>
+			<td><?php echo h($data['last_name']); ?></td>
+			<td><?php echo h($data['street']); ?></td>
+			<td><?php echo h($data['city']); ?></td>
+			<td><?php echo h($data['state']); ?></td>
+			<td><?php echo h($data['zip']); ?></td>
+			<td><?php echo h($data['phone']); ?></td>
+			<td><?php echo h($data['email']); ?></td>
+		</tr>
+</table>
+	
+	
+	
+	}
+		
 
